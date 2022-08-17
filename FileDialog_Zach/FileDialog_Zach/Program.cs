cbb5fffed5f559e7fae70157dc8857995ef5c2ce
14632 <14632@IT-SR19-010>
2022-08-17 14:38:44 +0200

    .

---
+++ FileDialog_Zach/FileDialog_Zach/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileDialog_Zach
+{
+    internal class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            string pathfile;
+
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Open Text File";
+            ofd.Filter = "TXT files|*.txt";
+            ofd.InitialDirectory = @"C:\";
+          
+            if (ofd.ShowDialog() != DialogResult.OK)
+            {
+                MessageBox.Show(ofd.FileName.ToString());
+                pathfile = ofd.FileName;
+            }
+            MessageBox.Show(ofd.FileName.ToString());
+
+            using(StreamReader sr = new StreamReader(ofd.FileName))
+            {
+                Console.WriteLine(sr.ReadToEnd());
+                sr.Close();
+            }
+            Console.ReadLine();
+        }
+    }
+}
