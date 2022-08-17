e5ed411e6da120afd2d3492d54e3faa10b9442a3
14632 <14632@IT-SR19-010>
2022-08-17 12:09:06 +0200

    push

---
+++ StreamReaderZach/StreamReaderZach/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StreamReaderZach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(@"C:\\Users\\14632\\Desktop\\testtext.txt");
+
+            using (StreamWriter sw = new StreamWriter(@"C:\\Users\\14632\\Desktop\\testtext4.txt"))
+            {
+                sw.Write(sr.ReadToEnd());
+                sw.WriteLine("\nThis is additional");
+            }
+            MessageBox.Show("Done!");
+        }
+    }
+}
